bb578721766c951605928c215c50083646f538f6
Miguel Raggi <mraggi@gmail.com>
2018-12-28 04:32:32 -0600

    Removed temp files

--- clang-format-helper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-fd --exclude external "\.[hc]pp" | xargs -d '\n' clang-format -i
-fd --exclude external "\.[hc]pp" | xargs -d '\n' sed -i 's/ \* /\*/g'
-fd --exclude external "\.[hc]pp" | xargs -d '\n' sed -i 's/ \/ /\//g'
-fd --exclude external "\.[hc]pp" | xargs -d '\n' sed -i 's/ \% /%/g'
-fd --exclude external "\.[hc]pp" | xargs -d '\n' sed -i 's/ \^ /^/g'
